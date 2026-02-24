b60d17ba941b442c56af1d5137bfd9d5283e9a88
Yann B-B <ybaglinbunod@gmail.com>
2026-02-24 14:02:49 -0800

    Initial Commit

---
+++ audio_listen/audio_listenApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_listenApp.swift
+//  audio_listen
+//
+//  Created by Yann Baglin-Bunod on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct audio_listenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ audio_listenTests/audio_listenTests.swift
@@ -0,0 +1,16 @@
+//
+//  audio_listenTests.swift
+//  audio_listenTests
+//
+//  Created by Yann Baglin-Bunod on 2/24/26.
+//
+
+import Testing
+
+struct audio_listenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
