b20d9434511c27c23bc5d199e230342b256a7ce6
Yann-b-b <ybaglinbunod@gmail.com>
2026-06-29 21:17:54 -0700

    refactor: remove dead Belt.color and Belt.symbolName

--- audio_listen/Presentation/Drill/Belt+UI.swift
+++ audio_listen/Presentation/Drill/Belt+UI.swift
@@ -1,20 +1,3 @@
-import SwiftUI
-
 extension Belt {
-    var color: Color {
-        switch self {
-        case .white: return Color(white: 0.85)
-        case .yellow: return .yellow
-        case .orange: return .orange
-        case .green: return .green
-        case .blue: return .blue
-        case .purple: return .purple
-        case .brown: return .brown
-        case .black: return .black
-        }
-    }
-
-    var symbolName: String { "medal.fill" }
-
     var assetName: String { "belt-\(displayName.lowercased())" }
 }
